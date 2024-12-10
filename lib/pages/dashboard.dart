a831e04897be5d95014b8689e1b72be50db1c85f
Tran Ngoc Bao <baotn@icd-vn.com>
2024-12-10 14:55:13 +0700

    wip

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -1,15 +1,39 @@
 import 'package:flutter/material.dart';
-
+import 'package:mysql1/mysql1.dart';
+import 'package:book_management/database.dart';
 class Dashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [Text('Dashboard')],
+      body: Padding (
+        padding: EdgeInsets.all(16),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Expanded(child: InfoCard()),
+            Expanded(child: InfoCard()),
+          ],
         ),
-      ),
+      )
+    );
+  }
+}
+
+class InfoCard extends StatelessWidget {
+  // final MySqlConnection db;
+
+  const InfoCard({
+    super.key
+    // required this.db,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding (
+        padding: const EdgeInsets.all(16),
+        child: Text('money')
+      )
     );
   }
 }
