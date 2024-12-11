ca988d4861263780921ef43e330bb254d97cc721
Tran Ngoc Bao <baotn@icd-vn.com>
2024-12-11 17:00:04 +0700

    upfata

--- lib/pages/products.dart
+++ lib/pages/products.dart
@@ -1,4 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:book_management/database.dart';
+import 'package:mysql1/mysql1.dart';
+
+Future getPorducts() async {
+  MySqlConnection db = await database();
+
+  String sql = '';
+  var results = await db.query(sql);
+
+  if(results.isNotEmpty) {
+    return results;
+  } else {
+    return null;
+  }
+}
 
 class Products extends StatelessWidget {
   @override
